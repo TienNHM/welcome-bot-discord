901fc75da93b5fccc1e1e4bb238c02cec9c9b3b4
Nguyen Huynh Minh Tien <33385777+TienNHM@users.noreply.github.com>
2021-10-04 23:55:45 +0700

    fix message

--- main.py
+++ main.py
@@ -13,12 +13,12 @@ class MyClient(discord.Client):
     async def on_member_join(self, member):
         guild = member.guild
         if guild.system_channel is not None:
-            msg = "📌 Để có thể truy cập vào tất cả các channel của discord, các bạn vui lòng đổi biệt danh theo định dạng:\n" + \
+            msg = "🥰 Để có thể truy cập vào tất cả các channel của discord, các bạn vui lòng đổi biệt danh theo định dạng:\n" + \
                 "```\n" + \
                 "Họ tên - MSSV - Lớp \n" + \
                 "``` \n" + \
-                "👉 Sau đó, nhớ điền form: https://tinyurl.com/Discord-K21-FITUTE để được set role nhé. \n\n " + \
-                "💥 Và đặc biệt, đừng quên đọc <#887981459749609492> nà. \n\n "
+                "👉 Sau đó, nhớ điền form: https://tinyurl.com/Discord-K21-FITUTE để được set role nhé. \n " + \
+                "💥 Và đặc biệt, đừng quên đọc <#887981459749609492> nà."
             embed = discord.Embed(timestamp=datetime.datetime.utcnow(),
                                   color=discord.Color.blue())
             # embed.add_field(name="Server created at",
@@ -27,9 +27,9 @@ class MyClient(discord.Client):
             # embed.add_field(name="Server Region", value=f"{guild.region}")
             # embed.add_field(name="Server ID", value=f"{guild.id}")
             embed.set_author(name=member.name, icon_url=member.avatar_url)
-            embed.add_field(name="Lưu ý:", value=f"💥 Chào mừng {member.mention} đến với Server {guild.name} nhé! \n\n{msg}")
+            embed.add_field(name="📌📌📌", value=f"💥 Chào mừng {member.mention} đến với Server {guild.name} nhé! \n\n{msg}")
             embed.set_thumbnail(
-                url=f"https://img.icons8.com/color/64/000000/commercial.png")
+                url=f"https://img.icons8.com/color/48/000000/commercial.png")
 
             await guild.system_channel.send(embed=embed)
 
