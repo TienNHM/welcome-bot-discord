579fc058aea118df376e02420ebbbb58faf907bb
Nguyen Huynh Minh Tien <33385777+TienNHM@users.noreply.github.com>
2021-10-05 00:13:28 +0700

    update VAR

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 MESSAGE = os.getenv('MESSAGE')
+CHANNEL_ID = os.getenv('CHANNEL_ID')
+THUMBNAIL = os.getenv('THUMBNAIL')
 
 class MyClient(discord.Client):
     async def on_ready(self):
@@ -14,12 +16,12 @@ class MyClient(discord.Client):
     async def on_member_join(self, member):
         guild = member.guild
         if guild.system_channel is not None:
-            msg = "🥰 Để có thể truy cập vào tất cả các channel của discord, các bạn vui lòng đổi biệt danh theo định dạng:\n" + \
+            msg = f"🥰 Để có thể truy cập vào tất cả các channel của discord, các bạn vui lòng đổi biệt danh theo định dạng:\n" + \
                 "```\n" + \
                 "Họ tên - MSSV - Lớp \n" + \
                 "``` \n" + \
-                "👉 Sau đó, nhớ điền form: https://tinyurl.com/Discord-K21-FITUTE để được set role nhé. \n " + \
-                "💥 Và đặc biệt, đừng quên đọc <#887981459749609492> nà."
+                "👉 Sau đó, nhớ điền form: https://tinyurl.com/Discord-K21-FITUTE để được set role nhé. \n " 
+                
             msg = msg if MESSAGE is None else MESSAGE
             embed = discord.Embed(timestamp=datetime.datetime.utcnow(),
                                   color=discord.Color.blue())
@@ -30,8 +32,8 @@ class MyClient(discord.Client):
             # embed.add_field(name="Server ID", value=f"{guild.id}")
             embed.set_author(name=member.name, icon_url=member.avatar_url)
             embed.add_field(name="🌟🌟🌟", value=f"\n💥 Chào mừng {member.mention} đến với Server {guild.name} nhé! \n\n{msg}")
-            embed.set_thumbnail(
-                url=f"https://img.icons8.com/color/48/000000/commercial.png")
+            embed.add_field(name="Lưu ý", value=f"Đừng quên đọc <#{CHANNEL_ID}> nhé!")
+            embed.set_thumbnail(url=THUMBNAIL)
 
             await guild.system_channel.send(embed=embed)
 
