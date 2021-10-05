50e114ceb97d6044de573ec597b22b2dfdf68c58
Nguyen Huynh Minh Tien <33385777+TienNHM@users.noreply.github.com>
2021-10-05 11:01:05 +0700

    remove code

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 MESSAGE = os.getenv('MESSAGE')
-# CHANNEL_ID = os.getenv('CHANNEL_ID')
 THUMBNAIL = os.getenv('THUMBNAIL')
 RULES = os.getenv('RULES')
 RULE_ICON = os.getenv('RULE_ICON')
@@ -19,16 +18,9 @@ class MyClient(discord.Client):
         await member.create_dm()
         
         #### WELCOME
-        msg = f"🥰 Để có thể truy cập vào tất cả các channel của discord, các bạn vui lòng đổi biệt danh theo định dạng:\n" + \
-            "```\n" + \
-            "Họ tên - MSSV - Lớp \n" + \
-            "``` \n" + \
-            "👉 Sau đó, nhớ điền form: https://tinyurl.com/Discord-K21-FITUTE để được set role nhé. \n "
-            
-        msg = msg if MESSAGE is None else MESSAGE
         embed = discord.Embed(timestamp=datetime.datetime.utcnow(),
                                 color=discord.Color.blue())
-        embed.add_field(name="🌟🌟🌟", value=f"\n💥 Chào mừng {member.mention} đến với Server {member.guild.name} nhé! \n\n{msg}")
+        embed.add_field(name="🌟🌟🌟", value=f"\n💥 Chào mừng {member.mention} đến với Server {member.guild.name} nhé! \n\n{MESSAGE}")
         embed.set_thumbnail(url=THUMBNAIL)
 
         await member.dm_channel.send(embed=embed)
